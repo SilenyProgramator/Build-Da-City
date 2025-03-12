88bbf432dbb9eecdc80362dd98fdfa209251f1dc
SilenyProgramator <joel.hardonus@gmail.com>
2025-03-12 10:17:55 +0100

    SPUST TO
    Allons enfant de la patrie
    Le jour de gloire est arrivé
    Contre nous de la tyrannie
    L'étendard sanglant est levé
    L'étendard sanglant est levé
    Entendez-vous dans les campagnes
    Mugir ces féroces soldats?
    Ils viennent jusque dans vos bras
    Égorger vos fils et vos compagnes
    Aux armes, citoyens (formez)
    Vos bataillons
    Marchons, oui, marchons
    Qu'un sang impur
    Abreuve nos sillons
    Sillons
    Ouh ouh

---
+++ # # Allons enfant de la patrie.py
@@ -0,0 +1,33 @@
+# # Allons enfant de la patrie
+# Le jour de gloire est arrivé
+# Contre nous de la tyrannie
+# L'étendard sanglant est levé
+# L'étendard sanglant est levé
+# Entendez-vous dans les campagnes
+# Mugir ces féroces soldats?
+# Ils viennent jusque dans vos bras
+# Égorger vos fils et vos compagnes
+# Aux armes, citoyens (formez)
+# Vos bataillons
+# Marchons, oui, marchons
+# Qu'un sang impur
+# Abreuve nos sillons
+# Sillons
+# Ouh ouh
+
+
+FR = True
+while FR == True:
+    a = input("je francie nejlepší?: ")
+
+
+
+
+    if a == "ano":
+        while FR == True:
+            print("VIVA LA FRANCE GHEGHEGHE")
+    elif a == "ne":
+        a = input("zkus to znovu: ")
+    else:
+        print("pico")
+        break
